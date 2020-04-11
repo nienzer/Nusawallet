4d9610d20455b594a4759ee1f3e7181781baed94
nienzer <dananienzin@gmail.com>
2020-04-11 16:06:11 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var walletconfig = {
+    token_contract: "0xDB2577af575664567b00222aBa2e266b75213C9E", // Dainet Token Contract address
+    token_decimals: 18,
+    token_symbol: 'NUSA',
+    token_name: 'Nusacoin',
+    etherscan_api: 'https://api.etherscan.io', // https://api.etherscan.io || https://ropsten.etherscan.io for test
+    etherscan_api_key: 'YourApiKey',
+    password: '', 
+    gas_price: 8,
+    gas_limit: 90000
+}
